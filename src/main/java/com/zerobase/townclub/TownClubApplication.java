37ead84e2ff595606f960736960450d3930a0b7f
UNA <lemonlee030@gmail.com>
2024-01-24 19:50:48 +0900

    chore: 프로젝트 셋업

---
+++ src/main/java/com/zerobase/townclub/TownClubApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.townclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TownClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TownClubApplication.class, args);
+    }
+
+}
